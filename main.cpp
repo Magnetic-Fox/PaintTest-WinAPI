74738119d9884da29a834f4b7af843738b14c9dd
Bartłomiej "Magnetic-Fox" Węgrzyn <magnetic-fox@hotmail.com>
2024-12-15 15:10:59 +0100

    Update main.cpp file

--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 
 #define WINCLASS    "TestWindow"
 
+HBRUSH g_hbrBackground=CreateSolidBrush(RGB(200,255,255));
+int width=640, height=480;
+
 ATOM registerMainWindowClass(WNDCLASS*);
 LRESULT CALLBACK MainWndProc(HWND, UINT, WPARAM, LPARAM);
 
@@ -13,13 +16,13 @@ ATOM registerMainWindowClass(HINSTANCE hInstance, WNDCLASS *wc) {
     wc->hInstance=      hInstance;
     wc->hIcon=          LoadIcon(NULL, IDI_ASTERISK);
     wc->hCursor=        LoadCursor(NULL, IDC_ARROW);
-    wc->hbrBackground=  (HBRUSH)(COLOR_WINDOW + 1);
+    wc->hbrBackground=  g_hbrBackground;
     wc->lpszMenuName=   NULL;
     wc->lpszClassName=  WINCLASS;
     return RegisterClass(wc);
 }
 
-void paintSomething(HWND hwnd, HDC hdc) {
+void paintSomething(HWND &hwnd, HDC &hdc) {
     POINT oldPoint;
     HPEN oldPen, redPen=CreatePen(PS_SOLID, 1, RGB(255,0,255));
     oldPen=(HPEN)SelectObject(hdc,redPen);
@@ -28,6 +31,27 @@ void paintSomething(HWND hwnd, HDC hdc) {
     SelectObject(hdc,oldPen);
     DeleteObject(redPen);
     Rectangle(hdc,20,20,300,300);
+    return;
+}
+
+void paintTheme(HDC &hdc, int &width, int &height) {
+    HBRUSH oldBrush;
+    HPEN oldPen, cyanPen=CreatePen(PS_SOLID, 1, RGB(0,80,80));
+    oldPen=(HPEN)SelectObject(hdc,cyanPen);
+    oldBrush=(HBRUSH)SelectObject(hdc,g_hbrBackground);
+    for(unsigned int y=0; y<height; y+=20) {
+        for(unsigned int x=0; x<width; x+=20) {
+            if(y%40==0) {
+                Rectangle(hdc, x, y, x+21, y+21);
+            }
+            else {
+                Rectangle(hdc, x+10, y, x+31, y+21);
+            }
+        }
+    }
+    SelectObject(hdc,oldPen);
+    DeleteObject(cyanPen);
+    SelectObject(hdc,oldBrush);
     return;
 }
 
@@ -66,16 +90,21 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
 LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     switch(msg) {
+        case WM_SIZE:
+            width= LOWORD(lParam);
+            height=HIWORD(lParam);
+            break;
         case WM_PAINT:
             PAINTSTRUCT ps;
             HDC hdc=BeginPaint(hwnd,&ps);
-            paintSomething(hwnd,hdc);
+            paintTheme(hdc,width,height);
             EndPaint(hwnd,&ps);
             break;
         case WM_CLOSE:
             DestroyWindow(hwnd);
             break;
         case WM_DESTROY:
+            DeleteObject(g_hbrBackground);
             PostQuitMessage(0);
             break;
         default:
